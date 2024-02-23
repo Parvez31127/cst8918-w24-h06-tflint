1f94ae98face6c6a004a6c9f15ce8c08cd7ead40
PARVEZ MALEK <parvez31127@gmail.com>
2024-02-22 20:36:35 -0500

    commit

--- variables.tf
+++ variables.tf
@@ -3,13 +3,12 @@ variable "labelPrefix" {
   type        = string
   description = "Your college username. This will form the beginning of various resource names."
 }
-
-variable "region" {
-  default = "westus3"
-}
-
 variable "admin_username" {
   type        = string
   default     = "azureadmin"
   description = "The username for the local user account on the VM."
+}
+variable "region" {
+  type    = string
+  default = "westus3"
 }
